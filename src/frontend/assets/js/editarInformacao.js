e16a8ce4373ab7e715ebcf75c67706990a8ab53a
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-06-21 09:29:10 -0300

    Padronização do código que edita informações

--- src/frontend/assets/js/editarInformacao.js
+++ src/frontend/assets/js/editarInformacao.js
@@ -1,7 +1,8 @@
+// Função que é responsável por editar as informações de uma tabela
 function editar(){
-
   // Obtém os parâmetros da URL atual
   var urlParams = new URLSearchParams(window.location.search);
+
   // Obtém o valor do parâmetro "id" que está na URL
   var id = urlParams.get('id');
   
@@ -9,73 +10,54 @@ function editar(){
   var url = '/tabela?id=' + id;
   
   // Obtém os elementos do DOM que serão preenchidos com as informações da tabela
-  const main = document.querySelector(".principal-detalhes-tabelas")
-  const titulo = document.querySelector("#titulo")
-  const descricao = document.querySelector("#descricao")
-  const conjuntoDados = document.querySelector("#conjunto-dados")
-  const owner = document.querySelector("#owner")
-  const steward = document.querySelector("#steward")
-  const ativo = document.querySelector("#ativo")
-  const tipoAtivo = document.querySelector("#tipo-ativo")
-  const database = document.querySelector("#database")
-  const caminhoTabela = document.querySelector("#caminho-tabela")
-  const ultimaAtualizacao = document.querySelector("#ultima-atualizacao")
-  const dataCriacao = document.querySelector("#data-criacao")
-  const defasagem = document.querySelector("#defasagem")
-  const frequenciaAtualizacao = document.querySelector("#frequencia-atualizacao")
-  const engenheiroIngestao = document.querySelector("#engenheiro-ingestao")
-  const sql_code = document.querySelector("#sql_code")
+  const titulo = document.querySelector("#titulo");
+  const descricao = document.querySelector("#descricao");
+  const conjuntoDados = document.querySelector("#conjunto-dados");
+  const owner = document.querySelector("#owner");
+  const steward = document.querySelector("#steward");
+  const database = document.querySelector("#database");
+  const caminhoTabela = document.querySelector("#caminho-tabela");
+  const defasagem = document.querySelector("#defasagem");
+  const engenheiroIngestao = document.querySelector("#engenheiro-ingestao");
+  const sql_code = document.querySelector("#sql_code");
   
   // Realiza uma requisição fetch para obter os dados da tabela
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      
+      // Armaena a tabela a partir dos dados retornados
       let tabela = data[0];
      
-      console.log(tabela)
-  
-      titulo.value = tabela.nome
-      descricao.value = tabela.descricao
-      conjuntoDados.value = tabela.categoria
-      owner.value = tabela.owner
-      steward.value = tabela.steward
-      // ativo.value = tabela.ativo
-      // tipoAtivo.value = tabela.tipoAtivo
-      database.value = tabela.database
-      caminhoTabela.value = tabela.caminho
-      // ultimaAtualizacao.value = tabela.ultimaAtualizacao
-      // dataCriacao.value = tabela.dataCriacao
-      defasagem.value = tabela.defasagem
-      // frequenciaAtualizacao.value = tabela.frequenciaAtualizacao
-      engenheiroIngestao.value = tabela.eng_ingestao
-  
+      // Preenche os elementos do DOM com as informações da tabela
+      titulo.value = tabela.nome;
+      descricao.value = tabela.descricao;
+      conjuntoDados.value = tabela.categoria;
+      owner.value = tabela.owner;
+      steward.value = tabela.steward;
+      database.value = tabela.database;
+      caminhoTabela.value = tabela.caminho;
+      defasagem.value = tabela.defasagem;
+      engenheiroIngestao.value = tabela.eng_ingestao;
     })
+    // Caso ocorra algum erro, exibe o erro no console
     .catch(error => {
       console.log(error);
     });
-  
   };
   
-  function enviar_solicitacao() {
-    const main = document.querySelector(".principal-detalhes-tabelas")
-    const titulo = document.querySelector("#titulo")
-    const descricao = document.querySelector("#descricao")
-    const conjuntoDados = document.querySelector("#conjunto-dados")
-    const owner = document.querySelector("#owner")
-    const steward = document.querySelector("#steward")
-    const ativo = document.querySelector("#ativo")
-    const tipoAtivo = document.querySelector("#tipo-ativo")
-    const database = document.querySelector("#database")
-    const caminhoTabela = document.querySelector("#caminho-tabela")
-    const ultimaAtualizacao = document.querySelector("#ultima-atualizacao")
-    const dataCriacao = document.querySelector("#data-criacao")
-    const defasagem = document.querySelector("#defasagem")
-    const frequenciaAtualizacao = document.querySelector("#frequencia-atualizacao")
-    const engenheiroIngestao = document.querySelector("#engenheiro-ingestao")
-  
-    
-    const sql_code = document.querySelector("#sql_code")
-    sql_code.value = `UPDATE tabela SET nome='${titulo.value}', descricao='${descricao.value}', categoria='${conjuntoDados.value}', owner='${owner.value}', steward='${steward.value}', database='${database.value}', caminho='${caminhoTabela.value}', defasagem='${defasagem.value}', eng_ingestao='${engenheiroIngestao.value}' WHERE id=61`
+  // Função que é responsável por enviar a solicitação de edição da tabela
+  function enviarSolicitacao() {
+    // Armazena os elementos do DOM que serão preenchidos com as informações da tabela
+    const titulo = document.querySelector("#titulo");
+    const descricao = document.querySelector("#descricao");
+    const conjuntoDados = document.querySelector("#conjunto-dados");
+    const owner = document.querySelector("#owner");
+    const steward = document.querySelector("#steward");
+    const database = document.querySelector("#database");
+    const caminhoTabela = document.querySelector("#caminho-tabela");
+    const defasagem = document.querySelector("#defasagem");
+    const engenheiroIngestao = document.querySelector("#engenheiro-ingestao");
+    const sql_code = document.querySelector("#sql_code");
+    sql_code.value = `UPDATE tabela SET nome='${titulo.value}', descricao='${descricao.value}', categoria='${conjuntoDados.value}', owner='${owner.value}', steward='${steward.value}', database='${database.value}', caminho='${caminhoTabela.value}', defasagem='${defasagem.value}', eng_ingestao='${engenheiroIngestao.value}' WHERE id=61`;
   }
   
